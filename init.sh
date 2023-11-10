547f70502dfceec66329e47ff8ee84fbdacc42c0
qq8244353 <qqq000a@outlook.com>
2023-11-10 16:00:39 +0900

    update script

--- init.sh
+++ init.sh
@@ -4,4 +4,9 @@ set -xe
 #sudo apt-get upgrade -y
 #sudo apt-get install libpcsclite-dev pcscd pcsc-tools -y
 
-sudo systemctl status pcscd.service pcscd.socket
+# sudo systemctl status pcscd.service pcscd.socket
+# enable access udev from non-root
+sudo sh -c 'echo SUBSYSTEM==\"usb\", ACTION==\"add\", ATTRS{idVendor}==\"054c\", ATTRS{idProduct}==\"06c3\", GROUP=\"plugdev\" >> /etc/udev/rules.d/nfcdev.rules'
+sudo udevadm control -R # then re-attach device
+sudo modprobe -r port100
+sudo sh -c 'echo blacklist port100 >> /etc/modprobe.d/blacklist-nfc.conf'
